76bad8eec1104047085d948b74952a6df53e86b3
John DiBaggio <jsantoku@gmail.com>
2014-08-27 16:45:24 -0500

    stream-server.js

--- stream-server.js
+++ stream-server.js
@@ -1,15 +1,55 @@
+var	BigBangClient = require("./BigBangClient.js"),
+	PewRuntime = require("./BigBangClient.js"),
+	bbNode = require("./NodeBigBangClient.js");
+
+var websocket = require('websocket');
+
+var client = new bbNode.NodeBigBangClient();
+var myChannel;
+
+client.connectAnonymous("thegigabots.app.bigbang.io:80", function(result) {
+	if( result.success) {
+		client.subscribe("newBot", function( err, c) {
+			if(!err) {
+				myChannel = c;
+				beginStream(client,c);
+			}
+			else {
+				console.log("Subscribe failure. " + err);
+			}
+		})
+	}
+	else {
+		console.log("Connect Failure.");
+	}
+});
+
+function beginStream(client, channel) {
+	console.log("Commence ze streaming now!!!");
+	/*
+	*TODO
+	*
+	*/
+
+
+}
+
+
+/*
+==========================================================*/
+
 
 if( process.argv.length < 3 ) {
 	console.log(
 		'Usage: \n' +
-		'node stream-server.js johnpi [8082 8084]'
+		'node stream-server.js <secret> [<stream-port> <websocket-port>]'
 	);
 	process.exit();
 }
 
-var STREAM_SECRET = process.argv[2],
+var STREAM_SECRET = process.argv[2] || 'johnpi',
 	STREAM_PORT = process.argv[3] || 8082,
-	WEBSOCKET_PORT = process.argv[4] || 8084,
+	WEBSOCKET_PORT = process.argv[4] || 80,
 	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
 
 var width = 320,
@@ -32,6 +72,8 @@ socketServer.on('connection', function(socket) {
 		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
 	});
 });
+
+//var socketServer = "thegigabots.app.bigbang.io";
 
 socketServer.broadcast = function(data, opts) {
 	for( var i in this.clients ) {
@@ -69,5 +111,17 @@ var streamServer = require('http').createServer( function(request, response) {
 	}
 }).listen(STREAM_PORT);
 
-console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/johnpi/'+width+'/'+height+'/');
-console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/');
+console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/'+STREAM_SECRET+'/'+width+'/'+height);
+//console.log('Awaiting WebSocket connections on ws://'+socketServer+':'+WEBSOCKET_PORT+'/');
+
+/*
+on raspberrypi, run:
+
+node /home/pi/dist/node/stream-server.js johnpi
+
+then:
+
+ffmpeg -s 320x240 -f video4linux2 -i /dev/video4 -f mpeg1video \ -b 800k -r 30 http://192.168.1.108:8082/johnpi/320/240/
+
+*/
+
--- stream.js
+++ stream.js
@@ -1,15 +1,37 @@
 /*
 * Live video-stream program for Rasp Pi
 */
-
-var websocket = require('websocket');
-
 var	BigBangClient = require("./BigBangClient.js"),
 	PewRuntime = require("./BigBangClient.js"),
 	bbNode = require("./NodeBigBangClient.js");
 
-var ffmpeg = require('fluent-ffmpeg');
-var express = require('express');
+var websocket = require('websocket'),
+	ffmpeg = require('fluent-ffmpeg'),
+	express = require('express'),
+	fs = require('fs');
+
+//var app = express();
+
+
+// create the target stream (can be any WritableStream)
+// var stream = fs.createWriteStream('/path/to/yout_target.flv')
+
+// // make sure you set the correct path to your video file
+// var proc = ffmpeg('/dev/video4')
+// 	// use the 'flashvideo' preset (located in /lib/presets/flashvideo.js)
+// 	.preset('flashvideo')
+// 	// setup event handlers
+// 	.on('end', function() {
+// 	console.log('file has been converted succesfully');
+// 	})
+// 	.on('error', function(err) {
+// 	console.log('an error happened: ' + err.message);
+// 	})
+// 	// save to stream
+// 	.pipe(stream, {end:true}); //end = true, close output stream after writing
+
+// console.dir(proc);
+// console.dir(stream);
 
 //var ffmpeg-node = require('./node_modules/ffmpeg-node/ffmpeg-node.js');
 
